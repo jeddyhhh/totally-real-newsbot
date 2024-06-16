8c68e565f876ae93fde7147ae8c056b0785014b4
jeddyhhh <59252043+jeddyhhh@users.noreply.github.com>
2024-06-16 16:48:44 +1000

    v0.2 - uses alltalkv2, hashtag generation etc.
    Adds:
    Hashtag gen
    Random RVC voice selection

--- run_bot.py
+++ run_bot.py
@@ -1,13 +1,11 @@
-## totally real newsbot - by jeddyh ##
+## totally real newsbot - by jeddyh - v0.2##
 import json
 import base64
-import schedule
 import requests
 import time
 import os
 import random
 import re
-import shutil
 from mutagen.wave import WAVE
 from PIL import Image
 from pathlib import Path
@@ -20,11 +18,19 @@ import time
 facebook_post_enable = True 
 combineVideos = True
 num_of_vids_to_combine = 3
-random_topic_mode = False
-random_article_select = False
-random_voice_mode = False
-cata_number = 0
-select_article = 1
+#random_voice_mode = False
+generate_hashtags = True
+
+## RVC Config ##
+rvc_enabled = True ## If false, it will just use the raw piper/coqui etc wav output
+default_rvc_voice = 'MadisonAllworth/model.pth' ## models are in alltalk/models/rvc_voices/*folder*/*model*.pth
+random_rvc_voice = False ## models must be in the rvc_voices.txt file in the root with the format '*folder*/*model*.pth' eg; peter/PeterGriffin.pth
+
+## NYT Article Selection Config ##
+cata_number = 0 ## Starts from 'arts' then 'automobiles' etc etc until it hits the end of the list and starts again.
+select_article = 1 ## Each catagory has an n number of articles associated with it, this selects the first one and moves to the second one once it goes through all the catagories. 
+random_topic_mode = True ## Picks a random topic for each generation. Ignores cata_number.
+random_article_select = True ## Looks up how many articles are associated with the catagory and picks a random one. Could be a new or old article. Ignores select_article.
 
 ## Image and Video Config ##
 image_height = "400"
@@ -43,7 +49,7 @@ ooba_headers = {"Content-Type": "application/json"}
 
 auto1111_url = 'http://127.0.0.1:7860/sdapi/v1/txt2img'
 
-tts_url = "http://127.0.0.1:7851/api/tts-generate-streaming"
+tts_url = "http://127.0.0.1:7851/api/tts-generate"
 tts_headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
 ## Social API Details ##
@@ -56,9 +62,11 @@ global textresult
 global latest_article
 video_array = []
 combined_title_desc = []
+hashtag_array = []
 vid_combine_counter = 0
 total_num_of_vids_gen = 0
 gen_info_array = []
+tts_output_file_path = ''
 
 class bcolors:
     HEADER = '\033[95m'
@@ -78,18 +86,20 @@ CATEGORIES = [
     "theater", "t-magazine", "upshot", "us", "world"
 ]
 
-DEFAULT_VOICES = ["female_01.wav","female_02.wav","female_03.wav","female_04.wav",
-                  "female_05.wav","female_06.wav","female_07.wav","male_01.wav",
-                  "male_02.wav","male_03.wav","male_04.wav","male_05.wav","arnold.wav",
-                  "Clint_Eastwood CC3 (enhanced).wav","David_Attenborough CC3.wav",
-                  "James_Earl_Jones CC3.wav","Morgan_Freeman CC3.wav"]
-
 def get_category_name(cata_number):
     return CATEGORIES[cata_number % len(CATEGORIES)]
 
-def getRandomVoice():
-    voiceCount = randrange(len(DEFAULT_VOICES))
-    return DEFAULT_VOICES[voiceCount % len(DEFAULT_VOICES)]
+## Coqui default voices - Will look into getting these working again, but for this project I prefer to use piper + RVC models.
+
+# DEFAULT_VOICES = ["female_01.wav","female_02.wav","female_03.wav","female_04.wav",
+#                   "female_05.wav","female_06.wav","female_07.wav","male_01.wav",
+#                   "male_02.wav","male_03.wav","male_04.wav","male_05.wav","arnold.wav",
+#                   "Clint_Eastwood CC3 (enhanced).wav","David_Attenborough CC3.wav",
+#                   "James_Earl_Jones CC3.wav","Morgan_Freeman CC3.wav"]
+
+# def getRandomVoice():
+#     voiceCount = randrange(len(DEFAULT_VOICES))
+#     return DEFAULT_VOICES[voiceCount % len(DEFAULT_VOICES)]
 
 def fetch_nyt_headline(cata_number, select_article):
     cata = get_category_name(cata_number)
@@ -140,6 +150,28 @@ def analyze_headline(headline):
     if response.status_code == 200:
         headline_tone = response.json()['choices'][0]['text'].split()[0]
         return headline_tone.strip()
+    
+def generate_article_hashtags(headline, article_desc):
+    prompt = f"Create 5 hashtags based on {headline} - {article_desc}, do not include anymore than 5 hashtags. Only include the answer.\nASSISTANT:"
+
+    request = {
+        'prompt': prompt,
+        'max_tokens': 40,
+        'do_sample': True,
+        'temperature': 1.1,
+        'top_p': 0.1,
+        'typical_p': 1,
+        'repetition_penalty': 1.18,
+        'top_k': 65,
+        'min_length': 16,
+        'num_beams': 1,
+        'add_bos_token': True,
+        'truncation_length': 2048
+    }
+
+    response = requests.post(ooba_url, headers=ooba_headers, json=request)
+    if response.status_code == 200:
+        return response.json()['choices'][0]['text'].strip()
 
 def generate_text(headline, article_desc):
     tone = get_random_line_from_txt("./emotions.txt")
@@ -169,25 +201,36 @@ def generate_text(headline, article_desc):
         return response.json()['choices'][0]['text'].strip()
 
 def generate_tts(article_text, file_name):
-    current_directory = os.getcwd()
-    if random_voice_mode == True:
-        random_voice = getRandomVoice()
-        data = {
-            'text': article_text,
-            'voice': f"{random_voice}",
-            'language': 'en',
-            'output_file': f"{current_directory}/outputs/{file_name}.wav"
-        }
-        print("Random voice enabled.")
-        print(f"Voice used: {random_voice}")
+    if rvc_enabled == True:
+        print(f"{bcolors.HEADER}RVC enabled{bcolors.ENDC}")
+        if random_rvc_voice == True:
+            rvc_voice = get_random_line_from_txt("./rvc_voices.txt")
+            print(f"{bcolors.HEADER}Random RVC voice enabled - Voice selected {rvc_voice}{bcolors.ENDC}")
+            form_data = {
+                'text_input': f'{article_text}',
+                'rvccharacter_voice_gen': f'{rvc_voice}',
+                'output_file_name': f'{file_name}'
+            }
+        else:
+            form_data = {
+                'text_input': f'{article_text}',
+                'rvccharacter_voice_gen': f'{default_rvc_voice}',
+                'output_file_name': f'{file_name}'
+            }
     else:
-        data = {
-            'text': article_text,
-            'voice': 'female_01.wav',
-            'language': 'en',
-            'output_file': f"{current_directory}/outputs/{file_name}.wav"
+        form_data = {
+            'text_input': f'{article_text}',
+            'rvccharacter_voice_gen': 'Disabled',
+            'output_file_name': f'{file_name}'
         }
-    requests.post(tts_url, headers=tts_headers, data=data)
+
+    tts_response = requests.post(tts_url, data=form_data, headers=tts_headers)
+
+    tts_response_json = tts_response.json()
+    tts_output_file_path = tts_response_json["output_file_path"]
+    print(tts_output_file_path)
+
+    return tts_output_file_path
 
 def save_encoded_image(b64_image, output_path):
     with open(output_path, "wb") as image_file:
@@ -208,8 +251,8 @@ def save_article_text(article_text, file_name, mode):
     with open(f'./outputs/{file_name}.txt', f"{mode}", encoding="utf-8") as file:
         file.write(article_text)
 
-def create_video_file(file_name):
-    audio_path = f"./outputs/{file_name}.wav"
+def create_video_file(file_name, tts_output_file_path):
+    audio_path = f'{tts_output_file_path}'
     images_path = "./outputs"
     
     audio = WAVE(audio_path)
@@ -236,15 +279,18 @@ def create_video_file(file_name):
     final_video = video.set_audio(audio) 
     final_video.write_videofile(fps=30, codec="libx264", verbose=False, filename=f"./videos/{file_name}_video.mp4")
 
-def upload_video_to_facebook(video_path, video_title, video_desc, combined_title_desc):
+def upload_video_to_facebook(video_path, video_title, video_desc, article_hashtags, combined_title_desc, hashtag_array):
     page_id = your_facebook_page_id
     fb_access_token = your_facebook_page_access_token
 
     if(combined_title_desc != ''):
         fb_desc = ''.join(str(x) for x in combined_title_desc)
+        hashtags = ''.join(str(y) for y in hashtag_array)
+
+        fb_desc = f"{fb_desc} \n {hashtags}"
 
     else:
-        fb_desc = f'{video_title} - {video_desc}'
+        fb_desc = f'{video_title} - {video_desc} - {article_hashtags}'
     
     files = {'source': open(video_path, 'rb')}
     post_url = f'https://graph-video.facebook.com/v20.0/{page_id}/videos'
@@ -271,7 +317,7 @@ while True:
     overall_tic = time.perf_counter()
     epoch_time = int(time.time())
 
-    print(f'\n{bcolors.WARNING}Facebook posting: {bcolors.ENDC}{bcolors.OKCYAN}{facebook_post_enable}{bcolors.ENDC}{bcolors.WARNING} - Combine videos: {bcolors.ENDC}{bcolors.OKCYAN}{combineVideos}{bcolors.ENDC}{bcolors.WARNING} - Random topic mode: {bcolors.ENDC}{bcolors.OKCYAN}{random_topic_mode}{bcolors.ENDC}{bcolors.WARNING} - Random article select: {bcolors.ENDC}{bcolors.OKCYAN}{random_article_select}{bcolors.ENDC}{bcolors.WARNING} - Random voice: {bcolors.ENDC}{bcolors.OKCYAN}{random_voice_mode}{bcolors.ENDC}')
+    print(f'\n{bcolors.WARNING}Facebook posting: {bcolors.ENDC}{bcolors.OKCYAN}{facebook_post_enable}{bcolors.ENDC}{bcolors.WARNING} - Combine videos: {bcolors.ENDC}{bcolors.OKCYAN}{combineVideos}{bcolors.ENDC}{bcolors.WARNING} - Random topic mode: {bcolors.ENDC}{bcolors.OKCYAN}{random_topic_mode}{bcolors.ENDC}{bcolors.WARNING} - Random article select: {bcolors.ENDC}{bcolors.OKCYAN}{random_article_select}{bcolors.ENDC}{bcolors.WARNING} - Random RVC mode: {bcolors.ENDC}{bcolors.OKCYAN}{random_rvc_voice}{bcolors.ENDC}')
     
     if random_topic_mode == True:
         cata_number = randrange(22)
@@ -301,6 +347,13 @@ while True:
 
     print(f'{bcolors.OKGREEN}Tone found: {headline_tone}. Time taken: {analyseHeadline_toc - analyseHeadline_tic:0.4f} seconds.{bcolors.ENDC}')
 
+    if generate_hashtags == True:
+        print(f'\n{bcolors.OKCYAN}Generating article hashtags...{bcolors.ENDC}')
+        genHashtags_tic = time.perf_counter()
+        article_hashtags = generate_article_hashtags(nyt_headline, nyt_article_desc)
+        genHashtags_toc = time.perf_counter()
+        print(f'{bcolors.OKGREEN}Hashtags generated - {article_hashtags.partition('\n')[0]} - Time taken: {genHashtags_toc - genHashtags_tic:0.4f}{bcolors.ENDC}')
+
     print(f'\n{bcolors.OKCYAN}Generating article text...{bcolors.ENDC}')
     genText_tic = time.perf_counter()
     generated_text = generate_text(nyt_headline, nyt_article_desc)
@@ -312,7 +365,7 @@ while True:
 
     print(f'\n{bcolors.OKCYAN}Generating text-to-speech...{bcolors.ENDC}')
     genTTS_tic = time.perf_counter()
-    generate_tts(generated_text[:1999], file_name)
+    tts_output_file_path = generate_tts(generated_text[:1999], file_name)
     genTTS_toc = time.perf_counter()
     genTTSTimeTaken = genTTS_toc - genTTS_tic
     gen_info_array.append(f"TTS gen time: {genTTSTimeTaken} seconds")
@@ -334,7 +387,7 @@ while True:
     
     print(f'\n{bcolors.OKCYAN}Creating video file...{bcolors.ENDC}')
     createVideo_tic = time.perf_counter()
-    create_video_file(file_name)
+    create_video_file(file_name, tts_output_file_path)
     createVideo_toc = time.perf_counter()
     print(f'{bcolors.OKGREEN}Creating video file complete. Time taken: {createVideo_toc - createVideo_tic:0.4f} seconds.{bcolors.ENDC}')
     
@@ -342,6 +395,10 @@ while True:
         video_array.append(f'./videos/{file_name}_video.mp4')
         combined_title_desc.append(f"{nyt_headline} - {nyt_article_desc}\n\n")
         vid_combine_counter += 1
+
+        if generate_hashtags == True:
+            formatted_hashtags = article_hashtags.partition('\n')[0]
+            hashtag_array.append(f"{formatted_hashtags.replace(",", "")}\n")
         
         if vid_combine_counter == num_of_vids_to_combine:
             print(f'\n{bcolors.WARNING}Combining videos has been enabled.{bcolors.ENDC}')
@@ -353,12 +410,13 @@ while True:
             if facebook_post_enable:
                 concat_filename = f'./combined/{epoch_time}.mp4'
                 print(f'\n{bcolors.OKCYAN}Posting to Facebook{bcolors.ENDC}')
-                upload_video_to_facebook(concat_filename, '', '', combined_title_desc)
+                upload_video_to_facebook(concat_filename, '', '', '', combined_title_desc, hashtag_array)
                 print(f'{bcolors.OKGREEN}Posting to Facebook complete.{bcolors.ENDC}')
                 combined_title_desc.clear()
+                hashtag_array.clear()
     else:
         if facebook_post_enable == True:
-            upload_video_to_facebook(f'./videos/{file_name}_video.mp4', nyt_headline, nyt_article_desc, '')
+            upload_video_to_facebook(f'./videos/{file_name}_video.mp4', nyt_headline, nyt_article_desc, article_hashtags, '', '')
             print(f'\n{bcolors.OKCYAN}Posting to Facebook{bcolors.ENDC}')
     
     total_num_of_vids_gen += 1
@@ -370,6 +428,7 @@ while True:
     save_article_text(f'\n\n{genInfo}', file_name, 'a')
     saveGenStats(f'\n\n{nyt_headline}\n{genInfo}')
     gen_info_array.clear()
+    tts_output_file_path = ''
 
     if random_article_select == False and random_topic_mode == False:
         cata_number = cata_number + 1
